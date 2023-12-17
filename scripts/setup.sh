9a54a87c752dc46c6132bdc52794774190fff361
Nadeesha Cabral <n@nadeesha.me>
2023-12-18 07:12:41 +1100

    fix: Better setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,4 +9,8 @@ API_SECRET=$(curl https://api.differential.dev/demo/token)
 # replace the API_SECRET with the string REPLACE_ME in src/d.ts file.
 sed -i '' "s/REPLACE_ME/$API_SECRET/g" src/d.ts
 
-echo "Setup complete!"
+echo "Setup complete! 🎉"
+
+echo "To play with the demo, run the following commands:"
+echo "  👉 Running the service: npm run service --name hello"
+echo "  👉 Running the consumer: npm run command --name greet"
