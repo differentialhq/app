8e6399cde75e26ba183cde612e1f3b353e7f1fed
Nadeesha Cabral <n@nadeesha.me>
2024-01-19 06:40:14 +1100

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -10,11 +10,3 @@ API_SECRET=$(curl https://api.differential.dev/demo/token)
 sed -i '' "s/REPLACE_ME/$API_SECRET/g" src/d.ts
 
 echo "Setup complete! 🎉"
-
-echo ""
-echo "To play with the demo, run the following commands in separate terminals:"
-echo "  👉 Running the service:"
-echo "        npm run service --name=hello"
-echo "  👉 Running the consumer:"
-echo "        npm run command --name=greet"
-echo ""
