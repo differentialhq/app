ac5f1ea80be197a46dca5449425a3a0253fbedc0
Nadeesha Cabral <n@nadeesha.me>
2024-03-01 06:10:19 +1100

    fix: Making sed cross platform

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,11 +6,11 @@ npm install
 echo "Obtaining API secret..."
 API_SECRET=$(curl https://api.differential.dev/demo/token)
 
-ls
-
-pwd
-
 # replace the API_SECRET with the string REPLACE_ME in src/d.ts file.
-sed -i '' "s/REPLACE_ME/$API_SECRET/g" src/d.ts
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sed -i "s/REPLACE_ME/$API_SECRET/g" src/d.ts
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/REPLACE_ME/$API_SECRET/g" src/d.ts
+fi
 
 echo "Setup complete! 🎉"
